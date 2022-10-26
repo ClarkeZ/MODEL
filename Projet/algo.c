848a3ff75a8b5b28a8dd1758d85f3c4090392a92
ClarkeZ <56111201+ClarkeZ@users.noreply.github.com>
2022-10-26 13:51:20 +0200

    Add files via upload

--- Projet/algo.c
+++ Projet/algo.c
@@ -14,6 +14,80 @@ double randfrom(double min, double max)
     return min + (rand() / div);
 }
 
-QR *qr_decomposition(Matrix *A){
+double func_sqrt(double n){
+    double sqrt = n / 2;
+    double tmp = 0;
+
+    while(sqrt != tmp){
+        tmp = sqrt;
+        sqrt = (n / tmp + tmp) / 2;
+    }
+
+    return sqrt;
+}
+
+double find_cos(int i, int j, Matrix *A){
+    if(A->m[i*A->n + j] == 0 && A->m[j*A->n + j] == 0)
+        return 1;
+    else   
+        return A->m[j*A->n + j] / sqrt(A->m[j*A->n + j] + A->m[i*A->n + i]);
+        // si sqrt de math.h ne marche pas utiliser la fonction ci-dessous
+        // return A->m[j*A->n + j] / func_sqrt(A->m[j*A->n + j] + A->m[i*A->n + i]);
+}
+
+double find_sin(int i, int j, Matrix *A){
+    if(A->m[i*A->n + j] == 0 && A->m[j*A->n + j] == 0)
+        return 1;
+    else   
+        return A->m[i*A->n + j] / sqrt(A->m[j*A->n + j] + A->m[i*A->n + i]);
+        //si sqrt de math.h ne marche pas utiliser la fonction ci-dessous
+        // return A->m[i*A->n + j] / func_sqrt(A->m[j*A->n + j] + A->m[i*A->n + i]);
+}
+
+Matrix *Givens(int i, int j, Matrix *A){
+    Matrix *G = init_eye(A->n);
     
+    double c = find_cos(i, j, A);
+    double s = find_sin(i, j, A);
+    
+    G->m[j*A->n + j] = c;
+    G->m[j*A->n + i] = s;
+    G->m[i*A->n + j] = 0 - s;
+    G->m[i*A->n + i] = c;
+
+    return G;
+}
+
+Matrix *mat_Q(Matrix *A){
+    Matrix *Q = init_eye(A->n);
+    Matrix *G = init_matrix(A->n);
+
+    for(int i = 0 ; i < A->n ; i++){
+        for(int j = 0 ; j < A->n ; j++){
+            G = Givens(i, j, A);
+            Q = matrix_mul(G, Q);
+            A = matrix_mul(G, A);
+        }
+    }
+
+    return Q;
+}
+
+QR *qr_decomposition(Matrix *A){
+    QR *qr = (QR *) malloc(sizeof(QR));
+    Matrix *Q = mat_Q(A);
+    Matrix *R = matrix_mul(Q, A);
+
+    for(int i = 0 ; i < A->n ; i++){
+        for(int j = 0; j < i ; j++){
+            R->m[i*A->n + j] = 0;
+        }
+    }
+
+    Q = matrix_transpose(Q);
+
+    qr->Q = Q;
+    qr->R = R;
+
+    return qr;
 }
--- Projet/algo.h
+++ Projet/algo.h
@@ -4,12 +4,25 @@
 #include <stdbool.h>
 #include <sys/time.h>
 #include <time.h>
+#include <math.h>
 
 #include "matrix.h"
 
 double wtime();
 
 double randfrom(double min, double max);
+
+double func_sqrt(double n);
+
+double find_cos(int i, int j, Matrix *A);
+
+double find_sin(int i, int j, Matrix *A);
+
+Matrix *Givens(int i, int j, Matrix *A);
+
+Matrix *mat_Q(Matrix *A);
+
+QR *qr_decomposition(Matrix *A);
 
 /* Multiplication d'une ligne de matrice par un coefficient */
 void mult_line_scalar(mpfr *line, mpfr coef, mpfr n);
@@ -20,5 +33,4 @@ void sub_lines(mpfr *line1, mpfr *line2, mpfr n);
 /* Verifie si la diagonale possede un coefficient nul */
 bool zero_in_diagonal(Matrix *A);
 
-QR *qr_decomposition(Matrix *A);
 
